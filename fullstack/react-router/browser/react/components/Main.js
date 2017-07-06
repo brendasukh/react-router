b36b3bef0b8aaa46e13b2f197b5a44b4a037dcad
brendasukh <sukhnyamdorj@Gantsetsegs-MacBook-Air.local>
2017-07-06 12:07:02 -0400

    single artist

--- fullstack/react-router/browser/react/components/Main.js
+++ fullstack/react-router/browser/react/components/Main.js
@@ -4,6 +4,7 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import Songs from './Songs';
 import { HashRouter, Route } from 'react-router-dom';
 import AllArtists from './AllArtists';
 import SingleArtist from './SingleArtist';
@@ -31,6 +32,8 @@ export default class Main extends Component {
             <Route path='/albums/:albumId' component={SingleAlbum} />
             <Route exact path='/artists' component={AllArtists} />
             <Route path='/artists/:artistId' component={SingleArtist} />
+            <Route path='/artists/:artistId/albums' component={SingleArtist}/>
+            <Route path='/artists/:artistId/songs' component={Songs}/>
           </div>
 
           <Player />
--- fullstack/react-router/browser/react/components/SingleArtist.js
+++ fullstack/react-router/browser/react/components/SingleArtist.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
+import Songs from './Songs'
 
 
 export default class SingleArtist extends Component {
@@ -16,29 +17,46 @@ export default class SingleArtist extends Component {
   componentDidMount () {
     const artistId=this.props.match.params.artistId;
 
-     axios.get(`/api/artists/${artistId}`)
+     const prom1=axios.get(`/api/artists/${artistId}`)
       .then(response => response.data)
       .then(data => {
-        this.setState({
+        return this.setState({
           artist: data.name
         });
       })
 
-      axios.get(`/api/artists/${artistId}/albums`)
+      const prom2=axios.get(`/api/artists/${artistId}/albums`)
       .then(response => response.data)
       .then(albums => {
-        this.setState({
+        // console.log(albums[0].songs)
+        return this.setState({
           albums: albums
         })
 
       })
+
+      const prom3=axios.get(`/api/artists/${artistId}/songs`)
+      .then(response => response.data)
+      .then(data => {
+        // console.log('data',data)
+        return this.setState({
+          songs: data
+        })
+      })
+
+      Promise.all([prom1, prom2,prom3])
+      .then(console.log('resolved'))
+
+      
   }
 
   render(){
 
     const artist = this.state.artist
     const albums = this.state.albums
-
+    const songs= this.state.songs
+    console.log('albums',albums)
+  
     return (
         <div>
             <h3>{ this.state.artist }</h3>
@@ -59,6 +77,7 @@ export default class SingleArtist extends Component {
                             <span>{ album.name }</span>
                           </h5>
                           <small>{ album.songs.length } songs</small>
+                
                         </div>
                         </Link>
                     </div>
@@ -68,9 +87,9 @@ export default class SingleArtist extends Component {
               </div>
 
             <h4>SONGS</h4>
-
-
-
+                
+              <Songs songs={songs} />
+            
         </div>
     )
 
